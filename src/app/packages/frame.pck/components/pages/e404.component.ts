ee9c592f81b0eb98173b9785d1112b0dca8fba89
skopatsikas <kopatsikas@tourismussuite.com>
2019-05-14 15:21:16 +0200

    add 404 page

--- src/app/packages/frame.pck/components/pages/e404.component.ts
+++ src/app/packages/frame.pck/components/pages/e404.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 
 export class E404Component {
+	public imageValue = Math.floor(Math.random() * 15) + 1 || 1;
+	public imageUrl = 'url(assets/images/auth/bg/bg_' + this.imageValue + '.jpg)';
 }
