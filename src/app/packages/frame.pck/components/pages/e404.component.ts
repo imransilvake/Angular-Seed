d7da942944685305dd03613edbf06e96b97a5f17
Imran Khan <imransilvake@gmail.com>
2019-05-14 15:45:23 +0200

    profile - 01

--- src/app/packages/frame.pck/components/pages/e404.component.ts
+++ src/app/packages/frame.pck/components/pages/e404.component.ts
@@ -1,6 +1,9 @@
 // angular
 import { Component } from '@angular/core';
 
+// app
+import { ROUTING } from '../../../../../environments/environment';
+
 @Component({
 	selector: 'app-e404',
 	templateUrl: './e404.component.html',
@@ -8,6 +11,7 @@ import { Component } from '@angular/core';
 })
 
 export class E404Component {
+	public routing = ROUTING;
 	public imageValue = Math.floor(Math.random() * 15) + 1 || 1;
 	public imageUrl = 'url(assets/images/auth/bg/bg_' + this.imageValue + '.jpg)';
 }
