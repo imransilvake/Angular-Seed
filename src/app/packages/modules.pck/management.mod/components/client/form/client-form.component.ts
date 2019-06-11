90af3c4a9a41301e3e13652379bdc0a53ecb5ba8
Imran Khan <imransilvake@gmail.com>
2019-06-11 15:49:33 +0200

    client - 10

--- src/app/packages/modules.pck/management.mod/components/client/form/client-form.component.ts
+++ src/app/packages/modules.pck/management.mod/components/client/form/client-form.component.ts
@@ -1,9 +1,11 @@
 // angular
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 // app
 import { ClientViewTypeEnum } from '../../../enums/client-view-type.enum';
 import { ClientViewInterface } from '../../../interfaces/client-view.interface';
+import { UserRoleEnum } from '../../../../authorization.mod/enums/user-role.enum';
+import { ClientService } from '../../../services/client.service';
 
 @Component({
 	selector: 'app-client-form',
@@ -11,9 +13,19 @@ import { ClientViewInterface } from '../../../interfaces/client-view.interface';
 	styleUrls: ['./client-form.component.scss']
 })
 
-export class ClientFormComponent {
+export class ClientFormComponent implements OnInit {
 	@Output() changeClientView: EventEmitter<any> = new EventEmitter();
 	@Input() id;
+
+	public currentUserRole: UserRoleEnum;
+
+	constructor(private _clientService: ClientService) {
+	}
+
+	ngOnInit() {
+		// set current user role
+		this.currentUserRole = this._clientService.appState.role;
+	}
 
 	/**
 	 * close client form
