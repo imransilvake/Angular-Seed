00b87f618bed2d47e6523b743c669e64d89f627b
Imran Khan <imransilvake@gmail.com>
2019-06-24 09:22:31 +0200

    user - 03

--- src/app/packages/modules.pck/management.mod/components/user/default/user-default.component.ts
+++ src/app/packages/modules.pck/management.mod/components/user/default/user-default.component.ts
@@ -1,5 +1,12 @@
 // angular
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+// app
+import { UserService } from '../../../services/user.service';
+import { AppViewTypeEnum } from '../../../enums/app-view-type.enum';
+import { UserViewInterface } from '../../../interfaces/user-view.interface';
 
 @Component({
 	selector: 'app-user-default',
@@ -7,7 +14,75 @@ import { Component } from '@angular/core';
 	styleUrls: ['./user-default.component.scss']
 })
 
-export class UserDefaultComponent {
-	constructor() {
+export class UserDefaultComponent implements OnInit, OnDestroy {
+	@Output() changeUserView: EventEmitter<any> = new EventEmitter();
+
+	public userOldAccountsList;
+	public newUsersTableApiUrl;
+	public existingUsersTableApiUrl;
+	public userNewRegistrationsList;
+
+	private _ngUnSubscribe: Subject<void> = new Subject<void>();
+
+	constructor(private _userService: UserService) {
+	}
+
+	ngOnInit() {
+		// set tables api
+		this.newUsersTableApiUrl = this._userService.userTablesServices.newUsers;
+		this.existingUsersTableApiUrl = this._userService.userTablesServices.existingUsers;
+
+		// listen: fetch new users & old user accounts list
+		this._userService.userDataEmitter
+			.pipe(takeUntil(this._ngUnSubscribe))
+			.subscribe(res => {
+				if (res && (res.newUsers || res.existingUsers)) {
+					// set new users list
+					this.userNewRegistrationsList = res.newUsers;
+
+					// set old user accounts list
+					this.userOldAccountsList = res.existingUsers;
+				}
+			});
+	}
+
+	ngOnDestroy() {
+		// remove subscriptions
+		this._ngUnSubscribe.next();
+		this._ngUnSubscribe.complete();
+	}
+
+	/**
+	 * registration: update table row
+	 *
+	 * @param id
+	 */
+	public onClickNewRegistrationFetchId(id?: string) {
+		// payload
+		const payload: UserViewInterface = {
+			view: AppViewTypeEnum.FORM,
+			id: id
+		};
+		this.changeUserView.emit(payload);
+	}
+
+	/**
+	 * create new user
+	 */
+	public onClickCreateNewUser() {
+	}
+
+	/**
+	 * old users: update table row
+	 *
+	 * @param id
+	 */
+	public onClickOldUserFetchId(id?: string) {
+		// payload
+		const payload: UserViewInterface = {
+			view: AppViewTypeEnum.FORM,
+			id: id
+		};
+		this.changeUserView.emit(payload);
 	}
 }
--- src/app/packages/modules.pck/management.mod/components/user/user.component.ts
+++ src/app/packages/modules.pck/management.mod/components/user/user.component.ts
@@ -1,5 +1,5 @@
 // angular
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { forkJoin, Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 import { NavigationEnd, Router } from '@angular/router';
@@ -16,14 +16,9 @@ import { AppViewTypeEnum } from '../../enums/app-view-type.enum';
 	styleUrls: ['./user.component.scss']
 })
 
-export class UserComponent implements OnInit, OnDestroy {
+export class UserComponent implements OnDestroy {
 	public pageView: AppViewTypeEnum = AppViewTypeEnum.DEFAULT;
 	public id;
-
-	public userOldAccountsList;
-	public newUsersTableApiUrl;
-	public existingUsersTableApiUrl;
-	public userNewRegistrationsList;
 
 	private _ngUnSubscribe: Subject<void> = new Subject<void>();
 
@@ -40,25 +35,6 @@ export class UserComponent implements OnInit, OnDestroy {
 				filter(event => event instanceof NavigationEnd)
 			)
 			.subscribe(() => this.triggerServices());
-	}
-
-	ngOnInit() {
-		// set tables api
-		this.newUsersTableApiUrl = this._userService.userTablesServices.newUsers;
-		this.existingUsersTableApiUrl = this._userService.userTablesServices.existingUsers;
-
-		// listen: fetch new users & old user accounts list
-		this._userService.userDataEmitter
-			.pipe(takeUntil(this._ngUnSubscribe))
-			.subscribe(res => {
-				if (res && (res.newUsers || res.existingUsers)) {
-					// set new users list
-					this.userNewRegistrationsList = res.newUsers;
-
-					// set old user accounts list
-					this.userOldAccountsList = res.existingUsers;
-				}
-			});
 	}
 
 	ngOnDestroy() {
@@ -87,29 +63,5 @@ export class UserComponent implements OnInit, OnDestroy {
 			// emit result
 			this._userService.userDataEmitter.next(result);
 		});
-	}
-
-	/**
-	 * registration: update table row
-	 *
-	 * @param id
-	 */
-	public onClickNewRegistrationFetchId(id?: string) {
-		console.log(id);
-	}
-
-	/**
-	 * create new user
-	 */
-	public onClickCreateNewUser() {
-	}
-
-	/**
-	 * old users: update table row
-	 *
-	 * @param id
-	 */
-	public onClickOldUserFetchId(id?: string) {
-		console.log(id);
 	}
 }
