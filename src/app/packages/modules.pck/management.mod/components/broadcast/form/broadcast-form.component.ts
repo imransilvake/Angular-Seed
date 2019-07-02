115fdb82bc1590b8f7a8785dce3ac531e8b47e71
Imran Khan <imransilvake@gmail.com>
2019-07-02 16:26:40 +0200

    broadcast - 2

--- src/app/packages/modules.pck/management.mod/components/broadcast/form/broadcast-form.component.ts
+++ src/app/packages/modules.pck/management.mod/components/broadcast/form/broadcast-form.component.ts
@@ -4,13 +4,17 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { I18n } from '@ngx-translate/i18n-polyfill';
 
 // app
 import * as moment from 'moment';
-import { UserService } from '../../../services/user.service';
 import { ProxyService } from '../../../../../core.pck/proxy.mod/services/proxy.service';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { ValidationService } from '../../../../../core.pck/fields.mod/services/validation.service';
+import { BroadcastInterface } from '../../../interfaces/broadcast.interface';
+import { BroadcastService } from '../../../services/broadcast.service';
+import { DialogTypeEnum } from '../../../../../utilities.pck/dialog.mod/enums/dialog-type.enum';
+import { DialogService } from '../../../../../utilities.pck/dialog.mod/services/dialog.service';
 
 @Component({
 	selector: 'app-broadcast-form',
@@ -33,7 +37,9 @@ export class BroadcastFormComponent implements OnInit, OnDestroy {
 		@Inject(MAT_DIALOG_DATA) public data: any,
 		public _dialogRef: MatDialogRef<BroadcastFormComponent>,
 		private _proxyService: ProxyService,
-		private _userService: UserService
+		private _broadcastService: BroadcastService,
+		private _i18n: I18n,
+		private _dialogService: DialogService
 	) {
 		// form group
 		this.formFields = new FormGroup({
@@ -52,10 +58,10 @@ export class BroadcastFormComponent implements OnInit, OnDestroy {
 			color: new FormControl('#71c578', [
 				Validators.required
 			]),
-			date:  new FormControl({ value: '', disabled: true }, [
+			date: new FormControl({ value: '', disabled: true }, [
 				Validators.required
 			]),
-			time:  new FormControl('', [
+			time: new FormControl('', [
 				Validators.required,
 				ValidationService.timeValidator
 			])
@@ -76,12 +82,12 @@ export class BroadcastFormComponent implements OnInit, OnDestroy {
 		}
 
 		// listen: form loading state
-		this._userService.formLoadingState
+		this._broadcastService.formLoadingState
 			.pipe(takeUntil(this._ngUnSubscribe))
 			.subscribe(() => this.loading = false);
 
 		// listen: error message
-		this._userService.errorMessage
+		this._broadcastService.errorMessage
 			.pipe(takeUntil(this._ngUnSubscribe))
 			.subscribe(res => {
 				// update loading state
@@ -134,7 +140,67 @@ export class BroadcastFormComponent implements OnInit, OnDestroy {
 	 */
 	public onSubmitForm() {
 		const formFields = this.formFields.getRawValue();
-		console.log(formFields);
+
+		// payload
+		const dialogPayload = {
+			type: DialogTypeEnum.CONFIRMATION,
+			payload: {
+				icon: 'dialog_confirmation',
+				title: this._i18n({
+					value: 'Title: Broadcast Create Confirmation',
+					id: 'Management_BroadcastCreateConfirmation_Form_Success_Title'
+				}),
+				message: this._i18n({
+					value: 'Description: Broadcast Create Confirmation',
+					id: 'Management_BroadcastCreateConfirmation_Form_Success_Description'
+				}),
+				buttonTexts: [
+					this._i18n({
+						value: 'Button - OK',
+						id: 'Common_Button_OK'
+					}),
+					this._i18n({
+						value: 'Button - Cancel',
+						id: 'Common_Button_Cancel'
+					}),
+				]
+			}
+		};
+
+		// dialog service
+		this._dialogService
+			.showDialog(dialogPayload)
+			.subscribe((res) => {
+				if (res) {
+					// concatenate date and time
+					let dateStr = formFields.date,
+						timeStr = formFields.time,
+						date = moment(dateStr),
+						time = moment(timeStr, 'HH:mm');
+
+					date.set({
+						hour: time.get('hour'),
+						minute: time.get('minute'),
+						second: time.get('second')
+					});
+
+					// utc format
+					const utc = date.utc().format();
+
+					// form payload
+					const formPayload: BroadcastInterface = {
+						Title: formFields.title,
+						Text: formFields.description,
+						Link: formFields.link,
+						Colour: formFields.color,
+						SendUser: this._broadcastService.currentUser.profile.email,
+						ExpDate: utc
+					};
+
+					// service
+					this._broadcastService.broadcastCreate(formPayload, this._dialogRef);
+				}
+			});
 	}
 
 	/**
--- src/app/packages/modules.pck/management.mod/services/broadcast.service.ts
+++ src/app/packages/modules.pck/management.mod/services/broadcast.service.ts
@@ -1,11 +1,15 @@
 // angular
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { BehaviorSubject, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { I18n } from '@ngx-translate/i18n-polyfill';
 
 // app
 import { AppOptions, AppServices } from '../../../../../app.config';
 import { ProxyService } from '../../../core.pck/proxy.mod/services/proxy.service';
+import { BroadcastInterface } from '../interfaces/broadcast.interface';
+import { DialogTypeEnum } from '../../../utilities.pck/dialog.mod/enums/dialog-type.enum';
+import { DialogService } from '../../../utilities.pck/dialog.mod/services/dialog.service';
 
 @Injectable()
 export class BroadcastService {
@@ -13,8 +17,14 @@ export class BroadcastService {
 	public appState;
 	public broadcastTablesServices;
 	public broadcastDataEmitter: BehaviorSubject<any> = new BehaviorSubject(0);
+	public errorMessage: EventEmitter<string> = new EventEmitter();
+	public formLoadingState: EventEmitter<boolean> = new EventEmitter();
 
-	constructor(private _proxyService: ProxyService) {
+	constructor(
+		private _proxyService: ProxyService,
+		private _i18n: I18n,
+		private _dialogService: DialogService
+	) {
 	}
 
 	/**
@@ -50,5 +60,38 @@ export class BroadcastService {
 		} else {
 			return of(null);
 		}
+	}
+
+	/**
+	 * create / resend broadcast
+	 *
+	 * @param formPayload
+	 * @param dialogRef
+	 */
+	public broadcastCreate(formPayload: BroadcastInterface, dialogRef: any) {
+		this._proxyService.postAPI(AppServices['Management']['Broadcast_Form_Create_All'], { bodyParams: formPayload })
+			.subscribe(() => {
+				// update loading state
+				this.formLoadingState.emit();
+
+				// payload
+				const dialogPayload = {
+					type: DialogTypeEnum.NOTICE,
+					payload: {
+						icon: 'dialog_tick',
+						title: this._i18n({ value: 'Title: Broadcast Created', id: 'Management_BroadcastCreated_Form_Success_Title' }),
+						message: this._i18n({
+							value: 'Description: Broadcast Created',
+							id: 'Management_BroadcastCreated_Form_Success_Description'
+						}),
+						buttonTexts: [this._i18n({ value: 'Button - OK', id: 'Common_Button_OK' })]
+					}
+				};
+
+				// dialog service
+				this._dialogService
+					.showDialog(dialogPayload)
+					.subscribe(() => dialogRef.close(true));
+			});
 	}
 }
