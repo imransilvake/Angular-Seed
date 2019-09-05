7e50a2d48dc64b3c71384066916d18e149f7dab4
Imran Khan <imransilvake@gmail.com>
2019-09-05 18:15:35 +0200

    updte

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // amplify
 import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 
 // app
 import { AppModule } from './app/app.module';
@@ -15,15 +16,7 @@ if (environment.production) {
 }
 
 // amplify
-Amplify.configure({
-	Auth: {
-		// REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-		identityPoolId: 'eu-central-1:16551f3d-72da-4362-8f7d-babb99e2ea6f',
-
-		// REQUIRED - Amazon Cognito Region
-		region: 'eu-central-1'
-	}
-});
+Amplify.configure(awsconfig);
 
 // bootstrap app
 platformBrowserDynamic().bootstrapModule(AppModule)
